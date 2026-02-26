4e646a436419e53ae63a390a215f2ae5e8bb1230
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-26 22:26:31 +0100

    styling(landing): Add Experience Elements to landing page

---
+++ src/app/pages/landing/experience-element/experience-element.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-element',
+  imports: [],
+  templateUrl: './experience-element.html',
+  styles: ``,
+})
+export class ExperienceElement {
+  badge = input.required<string>();
+  title = input.required<string>();
+  description = input.required<string>();
+}
--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component  } from '@angular/core';
-import { RouterLink, RouterLinkActive} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ExperienceElement } from './experience-element/experience-element';
 @Component({
   selector: 'app-landing',
-  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage, ExperienceElement],
   templateUrl: './landing.html',
   styles: ``,
 })
