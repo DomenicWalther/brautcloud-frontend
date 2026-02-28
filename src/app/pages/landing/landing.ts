afd4aef716d0bae687d40ee6afded5f995e77da8
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-28 01:17:23 +0100

    refactor(landing): Remove unused imports from landing.ts

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,6 +1,4 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import {LandingCtaSection} from './landing-cta-section/landing-cta-section';
 import {LandingExperienceSection} from './landing-experience-section/landing-experience-section';
 import {LandingHero} from './landing-hero/landing-hero';
